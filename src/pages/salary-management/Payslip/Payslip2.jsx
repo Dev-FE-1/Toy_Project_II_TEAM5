4f1b5e82bb1ea1dd12afe280918f7ce0efa2f533
Sumin <tnalsdldkr2@naver.com>
2024-07-28 00:12:58 +0900

    [#9] Feat: 급여 지급 내역 Ver.2 초기 UI 구현

--- src/pages/salary-management/Payslip/Payslip2.jsx
+++ src/pages/salary-management/Payslip/Payslip2.jsx
@@ -1,39 +1,66 @@
 import styled from 'styled-components'
 
-export default function Payslip() {
+export default function Payslip2() {
   const payrollData = {
-    month: 7,
     baseSalary: 3500000,
-    mealAllowance: 100000,
-    overtimePay: 120000,
-    longevityAllowance: 80000,
+    totalHours: 219,
+    regularHours: 144,
+    overtime: 75,
+    totalWorkdays: 18,
     bonus: 200000,
     totalPay: 3800000,
   }
 
+  const employeeData = {
+    name: '강호연',
+    id: 102034,
+    hireDate: 20150115,
+    payDay: '2024년 8월 16일',
+  }
+
   return (
     <PayslipContainer>
+      <Title>급여 명세서</Title>
       <TitleContainer>
-        <Title>급여 지급내역</Title>
-        <Month>{payrollData.month}월</Month>
+        <Company>
+          <h1>(주) Revive</h1>
+        </Company>
+        <Detail>
+          <EmployeeItem>
+            <EmployeeLabel>임직원 이름:&nbsp;</EmployeeLabel>
+            <EmployeeValue>{employeeData.name}</EmployeeValue>
+          </EmployeeItem>
+          <EmployeeItem>
+            <EmployeeLabel>사원번호:&nbsp;</EmployeeLabel>
+            <EmployeeValue>{employeeData.id}</EmployeeValue>
+          </EmployeeItem>
+          <EmployeeItem>
+            <EmployeeLabel>입사일:&nbsp;</EmployeeLabel>
+            <EmployeeValue>{employeeData.hireDate}</EmployeeValue>
+          </EmployeeItem>
+          <EmployeeItem>
+            <EmployeeLabel>급여지급일:&nbsp;</EmployeeLabel>
+            <EmployeeValue>{employeeData.payDay}</EmployeeValue>
+          </EmployeeItem>
+        </Detail>
       </TitleContainer>
       <DetailContainer>
         <PayrollItem>
-          <PayrollLabel>기본급</PayrollLabel>
-          <PayrollValue>{payrollData.baseSalary?.toLocaleString()} 원</PayrollValue>
+          <PayrollLabel>총 근로일수</PayrollLabel>
+          <PayrollValue>{payrollData.totalWorkdays.toLocaleString()} 일</PayrollValue>
         </PayrollItem>
         <PayrollItem>
-          <PayrollLabel>식대</PayrollLabel>
-          <PayrollValue>{payrollData.mealAllowance.toLocaleString()} 원</PayrollValue>
+          <PayrollLabel>정규 근로시간</PayrollLabel>
+          <PayrollValue>{payrollData.regularHours?.toLocaleString()} 시간</PayrollValue>
         </PayrollItem>
         <PayrollItem>
-          <PayrollLabel>연장수당</PayrollLabel>
-          <PayrollValue>{payrollData.overtimePay.toLocaleString()} 원</PayrollValue>
+          <PayrollLabel>연장 근로시간</PayrollLabel>
+          <PayrollValue>{payrollData.overtime?.toLocaleString()} 시간</PayrollValue>
         </PayrollItem>
-        <PayrollItem>
-          <PayrollLabel>근속수당</PayrollLabel>
-          <PayrollValue>{payrollData.longevityAllowance.toLocaleString()} 원</PayrollValue>
-        </PayrollItem>
+        <WorkHoursTotal>
+          <span>총 근로시간</span>
+          <span>{payrollData.totalHours?.toLocaleString()} 시간</span>
+        </WorkHoursTotal>
         <PayrollItem>
           <PayrollLabel>상여금</PayrollLabel>
           <PayrollValue>{payrollData.bonus.toLocaleString()} 원</PayrollValue>
@@ -51,6 +78,18 @@ export default function Payslip() {
   )
 }
 
+const Company = styled.div`
+  margin-bottom: 20px;
+`
+const Detail = styled.div``
+const EmployeeItem = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 5px;
+`
+const EmployeeLabel = styled.div``
+const EmployeeValue = styled.div``
+
 const PayslipContainer = styled.div`
   width: 573px;
   height: 868px;
@@ -62,19 +101,13 @@ const PayslipContainer = styled.div`
 
 const TitleContainer = styled.div`
   padding: 20px;
-  border-bottom: 2px solid #e0e0e0;
+  border-bottom: 1px solid #e0e0e0;
 `
 
 const Title = styled.div`
   font-size: 18px;
   font-weight: bold;
-  margin-bottom: 17px;
-`
-
-const Month = styled.div`
-  font-size: 32px;
-  font-weight: bold;
-  margin-bottom: 70px;
+  text-align: center;
 `
 
 const DetailContainer = styled.div`
@@ -83,7 +116,7 @@ const DetailContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  margin-bottom: 77px;
+  margin-bottom: 50px;
 `
 
 const PayrollItem = styled.div`
@@ -113,6 +146,18 @@ const PayrollTotal = styled.div`
   background-color: #f8fffe;
   padding: 8px 20px;
   font-weight: bold;
+`
+
+const WorkHoursTotal = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #f8fffe;
+  padding: 8px 20px;
+  font-weight: bold;
+  border-bottom: 2px solid #e0e0e0;
+  border-top: 1px solid #e0e0e0;
 `
 
 const PayrollActions = styled.div`
--- src/pages/salary-management/salary-management-page.jsx
+++ src/pages/salary-management/salary-management-page.jsx
@@ -1,9 +1,11 @@
 import Payslip from './Payslip/Payslip'
+import Payslip2 from './Payslip/Payslip2'
 function SalaryManagementPage() {
   return (
     <div>
       <h1>급여관리</h1>
-      <Payslip />
+      {/* <Payslip /> */}
+      <Payslip2 />
     </div>
   )
 }
