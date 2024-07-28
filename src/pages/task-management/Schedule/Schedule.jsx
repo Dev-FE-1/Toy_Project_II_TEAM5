165c28857c9fefb9ec45165aa1b0caaa81dc26e0
Sumin <tnalsdldkr2@naver.com>
2024-07-28 17:31:10 +0900

    [#10] Feat: Update Schedule Figma UI

--- src/pages/task-management/Schedule/Schedule.jsx
+++ src/pages/task-management/Schedule/Schedule.jsx
@@ -1,31 +1,37 @@
-import React from 'react'
 import styled from 'styled-components'
 
 const scheduleItems = [
-  { time: '06.00am', task: 'Spin Class', duration: '45min', color: 'green' },
-  { time: '08.00am', task: 'Meeting with PaperCup Cafe', duration: '30min', color: 'yellow' },
-  { time: '12.00am', task: 'Lunch with Esther Wallace', duration: '1hr', color: 'green' },
-  { time: '02.00pm', task: 'Call babysitter for Saturday', duration: '10min', color: 'green' },
-  { time: '02.30pm', task: 'Conference Call', duration: '1hr 30min', color: 'yellow' },
-  { time: '03.45pm', task: 'Pick up kids', duration: '30min', color: 'green' },
+  { time: '06.00am', task: 'Spin Class', duration: '45min', color: '#4fd1c5' },
+  { time: '08.00am', task: 'Meeting with PaperCup Cafe', duration: '30min', color: 'pink' },
+  { time: '12.00am', task: 'Lunch with Esther Wallace', duration: '1hr', color: '#4fd1c5' },
+  { time: '02.00pm', task: 'Call babysitter for Saturday', duration: '10min', color: 'orange' },
+  { time: '02.30pm', task: 'Conference Call', duration: '1hr 30min', color: '#760000' },
+  { time: '03.45pm', task: 'Pick up kids', duration: '30min', color: '#4fd1c5' },
+  { time: '04.45pm', task: 'Pick up kids', duration: '30min', color: '#4fd1c5' },
+  { time: '05.45pm', task: 'Pick up kids', duration: '30min', color: '#4fd1c5' },
 ]
 
 export default function Schedule() {
   return (
     <ScheduleContainer>
       <h1>Today Schedule</h1>
-      {scheduleItems.map((item, index) => (
-        <ScheduleItem key={index}>
-          <TimeContainer>
-            <DotContainer>
+      <ScheduleList>
+        {scheduleItems.map((item, index) => (
+          <ScheduleItem key={index}>
+            <DotLineContainer>
               <Dot color={item.color}></Dot>
-              <Time>{item.time}</Time>
-            </DotContainer>
-            <Duration>{item.duration}</Duration>
-          </TimeContainer>
-          <Task>{item.task}</Task>
-        </ScheduleItem>
-      ))}
+              <Line />
+            </DotLineContainer>
+            <ItemDetails>
+              <TimeContainer>
+                <Time>{item.time}</Time>
+                <Duration>{item.duration}</Duration>
+              </TimeContainer>
+              <Task>{item.task}</Task>
+            </ItemDetails>
+          </ScheduleItem>
+        ))}
+      </ScheduleList>
       <ButtonContainer>
         <AddButton>일정 추가</AddButton>
       </ButtonContainer>
@@ -45,42 +51,75 @@ const ScheduleContainer = styled.div`
   margin: 20px;
 `
 
+const ScheduleList = styled.div`
+  max-height: 510px;
+  overflow-y: auto;
+  width: 100%;
+  padding-right: 10px;
+`
+
 const ScheduleItem = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: column;
-  margin: 10px 0;
+  flex-direction: row;
+  /* margin: 10px 0; */
+  position: relative;
+  cursor: pointer;
 `
 
-const TimeContainer = styled.div`
+const DotLineContainer = styled.div`
+  height: auto;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-`
-const DotContainer = styled.div`
-  display: flex;
-  align-items: center;
+  position: relative;
+  margin-right: 10px;
 `
 
 const Dot = styled.div`
   width: 10px;
   height: 10px;
   border-radius: 50%;
-  margin-right: 10px;
   background-color: ${(props) => props.color};
+  z-index: 1;
+`
+
+const Line = styled.div`
+  width: 1px;
+  height: 100%;
+  background-color: #d2d2d2;
+  position: absolute;
+  top: 10px;
+`
+
+const ItemDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const TimeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
 `
 
 const Time = styled.div`
-  margin-right: 10px;
-`
-
-const Task = styled.div`
-  font-weight: 500;
+  color: #848484;
   margin-right: 10px;
 `
 
 const Duration = styled.div`
+  color: #848484;
   margin-right: 10px;
+`
+
+const Task = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  padding-bottom: 20px;
 `
 
 const ButtonContainer = styled.div`
