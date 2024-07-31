d0065086973c59b4ad386d4dbd4e576fea5d8bf9
wonjichoe <wonjichoe@gmail.com>
2024-07-31 17:48:19 +0900

    [#3] Design: Shadowy Box 공통 컴포넌트로 변경

--- src/components/List/EmployeeList.jsx
+++ src/components/List/EmployeeList.jsx
@@ -4,6 +4,7 @@ import { employees } from '../Constant/employees.jsx'
 import EmployeeProfile from './CustomRenderers/EmployeeProfile.jsx'
 import EmployeePosition from './CustomRenderers/EmployeePosition.jsx'
 import EmployeeStatus from './CustomRenderers/EmployeeStatus.jsx'
+import ShadowyBox from '@components/shared/ShadowyBox.jsx'
 
 function EmployeeList() {
   const headers = ['성명', '직책', '현황', '입사일']
@@ -28,12 +29,8 @@ function EmployeeList() {
   )
 }
 
-const ListContainer = styled.div`
+const ListContainer = styled(ShadowyBox)`
   width: 60%;
-  border-radius: 15px;
-  padding: 20px 15px;
-  box-shadow: 0 3.5px 5.5px rgba(0, 0, 0, 0.1);
-  margin-bottom: 10px;
 `
 
 export default EmployeeList
--- src/components/List/TodoList.jsx
+++ src/components/List/TodoList.jsx
@@ -4,6 +4,7 @@ import { todos } from '../Constant/todos.jsx'
 import TodoChecklist from './CustomRenderers/TodoChecklist.jsx'
 import TodoMore from './CustomRenderers/TodoMore.jsx'
 import Progressbar from './CustomRenderers/Progressbar.jsx'
+import ShadowyBox from '@components/shared/ShadowyBox.jsx'
 
 function ToDoList() {
   const headers = ['할 일', '상태', '완료율', '']
@@ -28,12 +29,8 @@ function ToDoList() {
   )
 }
 
-const ListContainer = styled.div`
+const ListContainer = styled(ShadowyBox)`
   width: 100%;
-  border-radius: 15px;
-  padding: 20px 15px;
-  box-shadow: 0 3.5px 5.5px rgba(0, 0, 0, 0.1);
-  margin-bottom: 10px;
 `
 
 export default ToDoList
