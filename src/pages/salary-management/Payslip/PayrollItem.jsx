86b0e4b7d55f78864a6d0b2a705dead955255897
Sumin <tnalsdldkr2@naver.com>
2024-07-31 17:44:22 +0900

    [#9] Feat: 연장수당 hover시 계산식 추가

--- src/pages/salary-management/Payslip/PayrollItem.jsx
+++ src/pages/salary-management/Payslip/PayrollItem.jsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
-const PayrollItem = ({ label, value }) => (
+const PayrollItem = ({ label, value, formula }) => (
   <PayrollItemContainer>
     <PayrollLabel>{label}</PayrollLabel>
     <PayrollValue>{value}</PayrollValue>
+    {formula && <Formula>{formula}</Formula>}
   </PayrollItemContainer>
 )
 
@@ -18,9 +19,9 @@ const PayrollItemContainer = styled.div`
   padding: 8px 40px;
   border-bottom: 1px solid #e0e0e0;
 
-  &:hover {
-    background-color: #f8fffe;
-    text-decoration: underline;
+  position: relative;
+  &:hover > div:last-child {
+    display: block;
   }
 `
 
@@ -31,4 +32,17 @@ const PayrollLabel = styled.div`
 
 const PayrollValue = styled.div`
   font-size: 16px;
+`
+const Formula = styled.div`
+  display: none;
+  position: absolute;
+  bottom: 0px;
+  left: 40px;
+  font-size: 12px;
+  color: #555;
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  padding: 5px;
+  border-radius: 4px;
+  white-space: nowrap;
 `
--- src/pages/salary-management/Payslip/Payslip.jsx
+++ src/pages/salary-management/Payslip/Payslip.jsx
@@ -5,14 +5,29 @@ import PayrollTotal from './PayrollTotal'
 import PayrollActions from './PayrollAction'
 
 export default function Payslip() {
+  const overtime = 16
+  const overtimeRate = 15822
+  const overtimeMultiplier = 1.5
+  const overtimePay = overtime * overtimeRate * overtimeMultiplier
+  const overtimeFormula = `연장 근무 시간(${overtime}시간) * 통상 시급(${overtimeRate.toLocaleString()}원) * ${overtimeMultiplier}`
+
   const payrollData = {
     month: 7,
     baseSalary: 3500000,
-    mealAllowance: 100000,
-    overtimePay: 120000,
-    longevityAllowance: 80000,
+    mealAllowance: 300000,
+    overtime,
+    overtimePay,
+    longevityAllowance: 200000,
     bonus: 200000,
-    totalPay: 3800000,
+    get totalPay() {
+      return (
+        this.baseSalary +
+        this.mealAllowance +
+        this.overtimePay +
+        this.longevityAllowance +
+        this.bonus
+      )
+    },
   }
 
   return (
@@ -22,15 +37,21 @@ export default function Payslip() {
         <Month>{payrollData.month}월</Month>
       </TitleContainer>
       <DetailContainer>
-        <PayrollItem label="기본급" value={`${payrollData.baseSalary?.toLocaleString()} 원`} />
-        <PayrollItem label="연장수당" value={`${payrollData.overtimePay.toLocaleString()} 원`} />
-        <PayrollItem
-          label="근속수당"
-          value={`${payrollData.longevityAllowance.toLocaleString()} 원`}
-        />
-        <PayrollItem label="식대" value={`${payrollData.mealAllowance.toLocaleString()} 원`} />
-        <PayrollItem label="상여금" value={`${payrollData.bonus.toLocaleString()} 원`} />
-        <PayrollTotal totalPay={payrollData.totalPay} />
+        {[
+          { label: '기본급', value: payrollData.baseSalary },
+          { label: '연장수당', value: payrollData.overtimePay, formula: overtimeFormula },
+          { label: '근속수당', value: payrollData.longevityAllowance },
+          { label: '식대', value: payrollData.mealAllowance },
+          { label: '상여금', value: payrollData.bonus },
+        ].map((item) => (
+          <PayrollItem
+            key={item.label}
+            label={item.label}
+            value={`${item.value.toLocaleString()} 원`}
+            formula={item.formula}
+          />
+        ))}
+        <PayrollTotal totalPay={payrollData.totalPay.toLocaleString()} />
       </DetailContainer>
       <PayrollActions />
     </PayslipContainer>
