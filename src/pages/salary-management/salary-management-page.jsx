fee3ad31c5d79a25760c59daf9bde78022041656
Sumin <tnalsdldkr2@naver.com>
2024-07-31 17:46:41 +0900

    [#9] Remove: Delete Payslip2.jsx

--- src/pages/salary-management/salary-management-page.jsx
+++ src/pages/salary-management/salary-management-page.jsx
@@ -1,13 +1,11 @@
 import { Headding } from '@pages/home/home-page'
 import Payslip from './Payslip/Payslip'
-import Payslip2 from './Payslip/Payslip2'
 
 function SalaryManagementPage() {
   return (
     <>
       <Headding>급여관리</Headding>
       <Payslip />
-      <Payslip2 />
     </>
   )
 }
