a6a8f74a822fa140abc9a6e9cb8137563e1271ef
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-08-01 01:13:06 +0900

    [#17] Fix: 절대경로 vite설정 라이브러리로 해결

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite'
+import jsconfigPaths from 'vite-jsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@mock': path.resolve(__dirname, 'src/mock'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@remote': path.resolve(__dirname, 'src/remote'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-    },
-  },
+  plugins: [react(), jsconfigPaths()],
 })
