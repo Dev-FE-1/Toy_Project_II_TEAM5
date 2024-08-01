a5b4aa7bb00cf605c6cd5f9ef6e1ff8d3f0872e6
Steven <ldh921126@gmail.com>
2024-08-02 08:58:23 +0900

    [partial #40} Feat: layout 변경진행, 업무관리 급여지급 페이지 크기가 맞지 않는 부분 수정 예정

--- src/components/Payslip/Payslip.jsx
+++ src/components/Payslip/Payslip.jsx
@@ -84,9 +84,9 @@ const Month = styled.div`
 
 const DetailContainer = styled.div`
   width: 100%;
-  height: 500px;
+  flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  margin-bottom: 30px;
+  margin-bottom: 20px;
 `
