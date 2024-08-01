d119b1203f5927f8e3d97f84a2ff52d51e089ff2
Steven <ldh921126@gmail.com>
2024-08-01 12:18:06 +0900

    0801임시저장

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
+// import React from 'react'
+import styled from 'styled-components'
 
 const Calendar = () => {
-  const daysInMonth = 31; 
-  const firstDayOfMonth = 1; 
+  const daysInMonth = 31
+  const firstDayOfMonth = 1
 
   const dummySchedules = {
-    2: [{ type: '출근', time: '09:00', description: "" }, { type: '퇴근', time: '18:00' , description: ""}],
-    3: [{ type: '출근', time: '09:00', description: "" }, { type: '퇴근', time: '18:00', description: "" }],
-    4: [{ type: '출근', time: '09:00' , description: ""}, { type: '퇴근', time: '18:00', description: "" }],
-    5: [{ type: '출근', time: '09:00', description: "" }, { type: '퇴근', time: '18:00' , description: ""}],
-    9: [{ type: '출근', time: '09:00' , description: ""}, { type: '퇴근', time: '18:00' , description: ""}],
-    10: [{ type: '출근', time: '09:00' , description: ""}, { type: '퇴근', time: '18:00', description: "" }],
-    11: [{ type: 'Development' , description: ""}],
-    12: [{ type: 'Edit file', description: "" }],
-    13: [{ type: 'Note taking' , description: ""}],
-  };
+    2: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    3: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    4: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    5: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    9: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    10: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    11: [{ type: 'Development', description: '' }],
+    12: [{ type: 'Edit file', description: '' }],
+    13: [{ type: 'Note taking', description: '' }],
+  }
 
   const renderCalendarDays = () => {
-    const days = [];
+    const days = []
     for (let i = 0; i < firstDayOfMonth - 1; i++) {
-      days.push(<DayCell key={`empty-${i}`} />);
+      days.push(<DayCell key={`empty-${i}`} />)
     }
     for (let day = 1; day <= daysInMonth; day++) {
       days.push(
@@ -34,10 +52,10 @@ const Calendar = () => {
             ))}
           </ScheduleList>
         </DayCell>
-      );
+      )
     }
-    return days;
-  };
+    return days
+  }
 
   return (
     <CalendarWrapper>
@@ -60,20 +78,20 @@ const Calendar = () => {
         </MonthSelector>
       </CalendarHeader>
       <CalendarGrid>
-        {['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'].map(day => (
+        {['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'].map((day) => (
           <DayHeader key={day}>{day}</DayHeader>
         ))}
         {renderCalendarDays()}
       </CalendarGrid>
     </CalendarWrapper>
-  );
-};
+  )
+}
 
 const CalendarWrapper = styled.div`
   font-family: Arial, sans-serif;
   max-width: 60%;
   margin: 0 auto;
-`;
+`
 
 const CalendarHeader = styled.div`
   display: flex;
@@ -81,45 +99,45 @@ const CalendarHeader = styled.div`
   align-items: center;
   padding: 20px;
   position: relative;
-`;
+`
 
 const MonthTitle = styled.h1`
   font-size: 24px;
-  position: absolute; 
+  position: absolute;
   left: 50%;
   transform: translateX(-50%);
-  margin: 0; 
-`;
+  margin: 0;
+`
 
 const MonthSelector = styled.select`
   justify-content: space-between;
   align-items: center;
-  padding: 10px; 
+  padding: 10px;
   font-weight: bold;
-  font-size: 16px; 
-  border: 1px solid #ccc; 
-  border-radius: 4px; 
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
   max-width: 7em;
-  width: 100%; 
-`;
+  width: 100%;
+`
 
 const MonthYear = styled.h2`
   padding-top: 30px;
   font-size: 24px;
-`;
+`
 
 const CalendarGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
 
   border: 1px solid #e9ecef;
-`;
+`
 
 const DayHeader = styled.div`
   padding: 10px;
 
   border: 1px solid #e9ecef;
-`;
+`
 
 const DayCell = styled.div`
   min-height: 100px;
@@ -128,21 +146,21 @@ const DayCell = styled.div`
   flex-direction: column;
   border: 1px solid #e9ecef;
   background-color: #fff;
-`;
+`
 
 const DayNumber = styled.div`
-  font-size:1.5em;
+  font-size: 1.5em;
   font-weight: bold;
   margin-bottom: 5px;
-`;
+`
 
 const ScheduleList = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-end; 
-  justify-content: flex-end; 
+  align-items: flex-end;
+  justify-content: flex-end;
   flex-grow: 1;
-`;
+`
 
 const ScheduleItem = styled.div`
   padding: 2px 5px;
@@ -155,17 +173,23 @@ const ScheduleItem = styled.div`
   text-overflow: ellipsis;
   /* word-break: break-all; 
   overflow-wrap: break-word; */
-  background-color: ${props => {
-    switch(props.type) {
-      case '출근': return '#FFA500';
-      case '퇴근': return '#FF6347';
-      case 'Development': return '#4682B4';
-      case 'Edit file': return '#9370DB';
-      case 'Note taking': return '#A9A9A9';
-      default: return '#ccc';
+  background-color: ${(props) => {
+    switch (props.type) {
+      case '출근':
+        return '#FFA500'
+      case '퇴근':
+        return '#FF6347'
+      case 'Development':
+        return '#4682B4'
+      case 'Edit file':
+        return '#9370DB'
+      case 'Note taking':
+        return '#A9A9A9'
+      default:
+        return '#ccc'
     }
   }};
   color: #fff;
-`;
+`
 
-export default Calendar;
+export default Calendar
