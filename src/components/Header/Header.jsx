a08c92a0fb3800fcb35ac66683a050a4e9d80e72
wonjichoe <wonjichoe@gmail.com>
2024-08-01 15:39:19 +0900

    [#25] Feat: Header 컴포넌트 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,22 @@
+import Flex from '@components/shared/Flex'
 import styled from 'styled-components'
 
 function Header({ header }) {
   return (
-    <HeaderBox>
+    <HeaderBox $justify="flex-start">
       <h1>{header}</h1>
     </HeaderBox>
   )
 }
 
-const HeaderBox = styled.div`
+const HeaderBox = styled(Flex)`
   width: 100%;
-  height: 90px;
+  height: 70px;
   background: #fff;
   border-radius: 15px;
-  font-size: 24px;
+  font-size: 20px;
   font-weight: 700;
+  padding-left: 25px;
 `
 
 export default Header
