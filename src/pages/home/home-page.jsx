20b75b1c7a770006d3593a366c9f0bd7bb409a22
Sumin <tnalsdldkr2@naver.com>
2024-08-01 18:32:22 +0900

    [#30] Remove: hedding 삭제

--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -29,16 +29,4 @@ const TopSection = styled.div`
   gap: 20px;
 `
 
-export const Headding = styled.h1`
-  display: block;
-  font-size: 2em;
-  color: ${colors.primary};
-  margin-block-start: 0.67em;
-  margin-block-end: 0.67em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-  padding-left: 40px;
-  font-weight: bold;
-  unicode-bidi: isolate;
-`
 export default HomePage
