c4dacd728ac6fe6508c59c1f476c8cdc7be6655e
Sumin <tnalsdldkr2@naver.com>
2024-08-03 19:10:05 +0900

    [#9] Feat: props를 DOM 요소에 직접 전달하려고 할 때 생기는 오류 해결

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -4,8 +4,8 @@ import { colors } from '@styles/Colors'
 const StyledButton = ({ text, backgroundColor, hoverColor, onClick, margin }) => {
   return (
     <Button
-      backgroundColor={backgroundColor}
-      hoverColor={hoverColor}
+      backgroundcolor={backgroundColor}
+      hovercolor={hoverColor}
       onClick={onClick}
       margin={margin}
     >
@@ -19,13 +19,13 @@ const Button = styled.button`
   padding: 9px 23.5px;
   border: none;
   border-radius: 8px;
-  background-color: ${(props) => props.backgroundColor};
+  background-color: ${(props) => props.backgroundcolor};
   margin: ${(props) => props.margin || '0'};
   color: ${colors.white};
   cursor: pointer;
 
   &:hover {
-    background-color: ${(props) => props.hoverColor};
+    background-color: ${(props) => props.hovercolor};
   }
 `
 
