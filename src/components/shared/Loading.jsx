f4fbb7962226d805bce828043efb8bd5554cfc21
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-08-04 22:20:26 +0900

    [#50] Feat: Loading 초안 완성

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -1,13 +1,39 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import Flex from './Flex'
+import PillIcon from '@assets/icons/pill.svg'
 
 function Loading() {
-  return <Container>Loading</Container>
+  return (
+    <Container>
+      <img src={PillIcon} alt="pill" />
+      <span>Loading...</span>
+    </Container>
+  )
 }
 
+const rotate = keyframes`
+ from {
+  transform: rotate(0deg)
+ }
+  to {
+   transform: rotate(360deg);
+  }
+ `
+
 const Container = styled(Flex)`
-  width: 30px;
-  height: 30px;
-  font-weight: bold;
+  flex-direction: column;
+  gap: 40px;
+  width: 200px;
+  & img {
+    width: 100%;
+    display: block;
+    animation: ${rotate} 1.2s ease 0s infinite;
+  }
+  & span {
+    display: inline;
+    font-size: 48px;
+    font-weight: bold;
+  }
 `
+
 export default Loading
