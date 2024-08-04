9f1da8c557e4e473f59e24e337d077175067c986
Kim Sumin <95954000+ssuminii@users.noreply.github.com>
2024-08-05 00:14:15 +0900

    Fix: 버튼 컴포넌트 DOM 에러 해결 (#54)
    * [#40] Feat: header 위에 여백 유무 주석 처리
    
    * [#9] Feat: 정정내역 모달 추가
    
    * [#9] Feat: props를 DOM 요소에 직접 전달하려고 할 때 생기는 오류 해결
    
    * [#9] Feat: props를 DOM 요소에 직접 전달하려고 할 때 생기는 오류 해결
    
    * [#41] Feat: Transient Props 사용
    
    * [#47] Feat: 정정내역 모달창 UI 구현
    
    * [#47] Refactor: 정정신청/정정내역 파일 분리
    
    * [#47] Refactor: PayrollAction.jsx 정정신청/정정내역 파일 분리
    
    * [#47] Feat: 정정내역 닫기 버튼 margin-bottom 수정
    
    * [#47] Feat: 정정내역 신청 완료 버튼 margin 수정

---
+++ src/components/Payslip/CorrectionTrigger.jsx
@@ -0,0 +1,25 @@
+import Modal from '@components/shared/Modal'
+import StyledButton from '@components/shared/Button'
+import { colors } from '@styles/Colors'
+import CorrectionModalContents from './CorrectionModalContents'
+
+function CorrectionTrigger() {
+  return (
+    <Modal Trigger={ButtonTrigger}>
+      <CorrectionModalContents />
+    </Modal>
+  )
+}
+
+function ButtonTrigger({ onClick }) {
+  return (
+    <StyledButton
+      onClick={onClick}
+      text="정정 내역"
+      backgroundColor={colors.primary_400}
+      hoverColor={colors.primary_500}
+    />
+  )
+}
+
+export default CorrectionTrigger
