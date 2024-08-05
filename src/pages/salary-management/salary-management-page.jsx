54ab1bb2180977746a72dcac8a21ed1e1eec0837
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-08-06 04:24:24 +0900

    [close: #56] Config: 급여관리  페이지와 업무관리 페이지를 위한  컨테이너 컴포넌트 제작 (Context API)

--- src/pages/salary-management/salary-management-page.jsx
+++ src/pages/salary-management/salary-management-page.jsx
@@ -1,8 +1,24 @@
 import Calendar from '@components/Calendar/Calendar'
+import CalendarContextProvider from '@components/Container/calendar-context'
+import Footer from '@components/Footer/Footer'
 import Header from '@components/Header/Header'
 import Payslip from '@components/Payslip/Payslip'
 import styled from 'styled-components'
-import Footer from '@components/Footer/Footer'
+
+function SalaryManagementPage() {
+  return (
+    <PageContainer>
+      <Header header="급여 내역" />
+      <CalendarContextProvider>
+        <ContentContainer>
+          <Calendar />
+          <Payslip />
+        </ContentContainer>
+      </CalendarContextProvider>
+      <Footer />
+    </PageContainer>
+  )
+}
 
 const PageContainer = styled.div`
   /* padding: 50px 20px 0 0; */
@@ -14,29 +30,7 @@ const PageContainer = styled.div`
 
 const ContentContainer = styled.div`
   display: flex;
-  flex-direction: row;
-  flex: 1;
   margin: 20px 0;
   gap: 20px;
 `
-const StyledCalendar = styled(Calendar)`
-  width: 90%;
-`
-
-const StyledPayslip = styled(Payslip)`
-  width: 10%;
-`
-
-function SalaryManagementPage() {
-  return (
-    <PageContainer>
-      <Header header="급여 내역" />
-      <ContentContainer>
-        <StyledCalendar />
-        <StyledPayslip />
-      </ContentContainer>
-      <Footer />
-    </PageContainer>
-  )
-}
 export default SalaryManagementPage
--- src/pages/task-management/taskManagement-page.jsx
+++ src/pages/task-management/taskManagement-page.jsx
@@ -1,8 +1,24 @@
-import Header from '@components/Header/Header'
 import Calendar from '@components/Calendar/Calendar'
+import CalendarContextProvider from '@components/Container/calendar-context'
+import Footer from '@components/Footer/Footer'
+import Header from '@components/Header/Header'
 import Schedule from '@components/Schedule/Schedule'
 import styled from 'styled-components'
-import Footer from '@components/Footer/Footer'
+
+function TaskManagementPage() {
+  return (
+    <PageContainer>
+      <Header header="업무 관리" />
+      <CalendarContextProvider>
+        <ContentContainer>
+          <Calendar />
+          <Schedule />
+        </ContentContainer>
+      </CalendarContextProvider>
+      <Footer />
+    </PageContainer>
+  )
+}
 
 const PageContainer = styled.div`
   display: flex;
@@ -18,24 +34,4 @@ const ContentContainer = styled.div`
   margin: 20px 0;
   gap: 20px;
 `
-const StyledCalendar = styled(Calendar)`
-  width: 90%;
-`
-
-const StyledSchedule = styled(Schedule)`
-  width: 10%;
-`
-
-function TaskManagementPage() {
-  return (
-    <PageContainer>
-      <Header header="업무 관리" />
-      <ContentContainer>
-        <StyledCalendar />
-        <StyledSchedule />
-      </ContentContainer>
-      <Footer />
-    </PageContainer>
-  )
-}
 export default TaskManagementPage
