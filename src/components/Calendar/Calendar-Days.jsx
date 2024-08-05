ce823619a0c9ce0450869cfa4744e45ba84d0d48
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-08-06 04:25:33 +0900

    [#4] Refactor: Calendar 컴포넌트 리팩토링

---
+++ src/components/Calendar/Calendar-Days.jsx
@@ -0,0 +1,115 @@
+import { CalendarContext } from '@components/Container/calendar-context'
+import useCalendar from '@hooks/useCalendar'
+import { colors } from '@styles/Colors'
+import { useContext } from 'react'
+import styled, { css } from 'styled-components'
+
+function CalendarDays() {
+  const { month, year } = useContext(CalendarContext)
+
+  const { calendarDays } = useCalendar(year, month)
+  // console.log('d', calendarDays)
+
+  const dummySchedules = {
+    2: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    3: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    4: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    5: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    9: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    10: [
+      { type: '출근', time: '09:00', description: '' },
+      { type: '퇴근', time: '18:00', description: '' },
+    ],
+    11: [{ type: 'Development', description: '' }],
+    12: [{ type: 'Edit file', description: '' }],
+    13: [{ type: 'Note taking', description: '' }],
+  }
+
+  const isLastMonth = (day) => day.getMonth() !== month
+
+  return calendarDays.map((day) => (
+    <Container key={day} isLastMonth={isLastMonth(day)}>
+      <span className="day">{day.getDate()}</span>
+      <ScheduleList>
+        {dummySchedules[day.getDate()]?.map((schedule, index) => (
+          <ScheduleItem key={index} type={schedule.type}>
+            {schedule.type} {schedule.time}
+          </ScheduleItem>
+        ))}
+      </ScheduleList>
+    </Container>
+  ))
+}
+
+const Container = styled.div`
+  min-height: 130px;
+  min-width: 130px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #e9ecef;
+  background-color: #fff;
+
+  .day {
+    font-size: 1.5em;
+    font-weight: bold;
+    margin-bottom: 5px;
+    ${({ isLastMonth }) =>
+      isLastMonth &&
+      css`
+        color: ${colors.gray};
+        font-weight: 400;
+      `}
+  }
+`
+
+const ScheduleList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  gap: 4px;
+  flex-grow: 1;
+`
+const ScheduleItem = styled.div`
+  padding: 6px 8px;
+  max-width: 70%;
+  border-radius: 3px;
+  font-size: 12px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  background-color: ${(props) => {
+    switch (props.type) {
+      case '출근':
+        return '#FFA500'
+      case '퇴근':
+        return '#FF6347'
+      case 'Development':
+        return '#4682B4'
+      case 'Edit file':
+        return '#9370DB'
+      case 'Note taking':
+        return '#A9A9A9'
+      default:
+        return '#ccc'
+    }
+  }};
+  color: #fff;
+`
+export default CalendarDays
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,99 +1,33 @@
-import {
-  CalendarWrapper,
-  CalendarHeader,
-  MonthTitle,
-  MonthSelector,
-  MonthYear,
-  CalendarGrid,
-  DayHeader,
-  DayCell,
-  DayNumber,
-  ScheduleList,
-  ScheduleItem,
-} from './Calendar.styled'
-/*
-필요한 데이터:
-월별 날짜(30,31...)
-시작하는 요일 -> state
+import ShadowyBox from '@components/shared/ShadowyBox'
+import CalendarDays from './Calendar-Days'
+import CalendarHeader from './Calendar-header'
+import CalendarNavigation from './Calendar-Navigation'
+import styled from 'styled-components'
+import { colors } from '@styles/Colors'
 
-*/
 const Calendar = () => {
-  const weekDay = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-  const daysInMonth = 31
-  const firstDayOfMonth = weekDay.indexOf('TUE')
-
-  const dummySchedules = {
-    2: [
-      { type: '출근', time: '09:00', description: '' },
-      { type: '퇴근', time: '18:00', description: '' },
-    ],
-    3: [
-      { type: '출근', time: '09:00', description: '' },
-      { type: '퇴근', time: '18:00', description: '' },
-    ],
-    4: [
-      { type: '출근', time: '09:00', description: '' },
-      { type: '퇴근', time: '18:00', description: '' },
-    ],
-    5: [
-      { type: '출근', time: '09:00', description: '' },
-      { type: '퇴근', time: '18:00', description: '' },
-    ],
-    9: [
-      { type: '출근', time: '09:00', description: '' },
-      { type: '퇴근', time: '18:00', description: '' },
-    ],
-    10: [
-      { type: '출근', time: '09:00', description: '' },
-      { type: '퇴근', time: '18:00', description: '' },
-    ],
-    11: [{ type: 'Development', description: '' }],
-    12: [{ type: 'Edit file', description: '' }],
-    13: [{ type: 'Note taking', description: '' }],
-  }
-
-  const renderCalendarDays = () => {
-    const emptyCells = Array.from({ length: firstDayOfMonth }).map((_, index) => (
-      <DayCell key={`empty-${index}`} />
-    ))
-
-    const days = Array.from({ length: daysInMonth }).map((_, day) => (
-      <DayCell key={day + 1}>
-        <DayNumber>{day + 1}</DayNumber>
-        <ScheduleList>
-          {dummySchedules[day + 1]?.map((schedule, index) => (
-            <ScheduleItem key={index} type={schedule.type}>
-              {schedule.type} {schedule.time}
-            </ScheduleItem>
-          ))}
-        </ScheduleList>
-      </DayCell>
-    ))
-
-    return [...emptyCells, ...days]
-  }
-
   return (
-    <CalendarWrapper>
-      <CalendarHeader>
-        <MonthYear>July, 2024</MonthYear>
-        <MonthTitle>7월</MonthTitle>
-        <MonthSelector>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <option key={index + 1} value={index + 1}>
-              {index + 1}월
-            </option>
-          ))}
-        </MonthSelector>
-      </CalendarHeader>
-      <CalendarGrid>
-        {weekDay.map((day) => (
-          <DayHeader key={day}>{day}</DayHeader>
-        ))}
-        {renderCalendarDays()}
-      </CalendarGrid>
-    </CalendarWrapper>
+    <Container>
+      <CalendarNavigation />
+      <CalendarContents>
+        <CalendarHeader />
+        <CalendarDays />
+      </CalendarContents>
+    </Container>
   )
 }
+
+const Container = styled(ShadowyBox)`
+  padding: 20px 50px;
+  padding-bottom: 0;
+  background-color: ${colors.white};
+  flex-grow: 1;
+`
+
+const CalendarContents = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  border: 1px solid #e9ecef;
+`
 
 export default Calendar
