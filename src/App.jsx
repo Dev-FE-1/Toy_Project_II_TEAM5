c8cc1f936a6ab9a551d18d1e2f2c1f99407f6c45
LeeYunHwan <cyzhqly@gmail.com>
2024-08-06 14:59:48 +0900

    [#28] Fix: 잘못된 페이지 이동 코드 변경 및 404 페이지 연동

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,12 @@ import SalaryManagementPage from '@pages/salary-management/salary-management-pag
 import SigninPage from '@pages/signin/signin-page'
 import TaskManagementPage from '@pages/task-management/taskManagement-page'
 import TestPage from '@pages/test/test-page'
-import { useEffect } from 'react'
 import useAuthState from '@components/Login/useAuthState'
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Loading from '@components/shared/Loading'
 
 function App() {
-  const navigate = useNavigate()
-  const location = useLocation()
   const { user, loading } = useAuthState()
-  const pageLists = ['/', '/salary', '/task', '/test', '/signin', '*']
-
-  useEffect(() => {
-    if (loading) return
-    const curPage = pageLists.find((pageList) => pageList === location.pathname)
-    //임의로 페이지를 옮겼을 때, user 정보가 없으면 signin/ 있으면 마지막페이지로 이동
-    if (!user) {
-      navigate('/signin')
-    } else {
-      if (location.pathname === '/signin' || !curPage) {
-        navigate(-1)
-      }
-    }
-  }, [navigate, location.pathname, user, loading])
 
   if (loading) {
     return <Loading />
@@ -43,7 +26,7 @@ function App() {
           <Route path="/test" element={<TestPage />} />
           <Route path="/admin" element={<AdminPage />} />
         </Route>
-        <Route path="/signin" element={<SigninPage />} />
+        {!user && <Route path="/signin" element={<SigninPage />} />}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
