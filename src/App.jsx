2a1d1c57b9e49db1108e90effdc917fc843016a4
LeeYunHwan <86473590+unanbb@users.noreply.github.com>
2024-08-08 22:46:33 +0900

    Feat: 페이지 접근 시 오류 및 페이지 라우팅 로직 수정 (#74)
    * [#7] Feat: Login 페이지 background 패턴 이미지 변경
    
    * [#7] Feat: Login 페이지 구조 수정
    
    * [#7] Feat: Login 페이지 요소 margin 값 변경
    
    * [#28] Feat: Login 페이지에 firebase auth 설정 및 연동
    
    * [#28] Feat: 로그인 검사 코드 작성 및 실패 시 에러 메시지 구현
    
    * [#28] Feat: 로그인/로그아웃 라우팅 구현
    
    * [#28] Feat: 로그인 정보 있을 때, Login 페이지 임의 이동 불가 코드 작성
    
    * [#7] Design: 불필요한 styled component 삭제 및 기존 component에 병합
    
    * [#7] Fix: 병합 도중 중복 코드 삭제
    
    * [#28] Rename: 함수명 수정
    
    * [#28] Feat: 로그인 시 user 데이터 저장 방식 세션 스토리지로 변경
    
    * [#28] Feat: Login 페이지 Input 태그 외부 컴포넌트화
    
    * [#28] Feat: Alert 컴포넌트 고정 공간 설정
    
    * [#28] Fix: useEffect 무한루프 오류 해결
    
    * [#28] Fix: Alert 컴포넌트 props 콘솔 오류 수정
    
    * [#28] Feat: 로그아웃/유저 검증 훅 생성 및  로그인 방식 파이어베이스 auth로 변경
    
    * [#28] Fix: 로그인 이후 새로고침 시 로그인 페이지로 튕기는 현상 수정
    
    * [#28] Feat: 없는 페이지로 이동 시 페이지 되돌리기 설정
    
    * [#28] Feat: user 정보 저장 방식 onAuthStateChanged로 수정 및 새로고침 시 Loading 창 연동
    
    * [#28] Fix: 잘못된 페이지 이동 코드 변경 및 404 페이지 연동
    
    * [#28] Fix: 로그인 에러 메시지 미출력 오류 수정
    
    * [#28] Feat: (개발 편의성) 로그인 검사 함수 주석 처리
    
    * [#28] Rename: 로그인 관련 hook 폴더 변경
    
    * [#28] Feat: 이메일 기억하기 토글 기능 구현
    
    * [#28] Feat: email 값 로컬스토리지에 실시간으로 받아오는 코드 작성
    
    * [#28] Design: 로그인 버튼 :active 및 transition 효과 추가
    
    * [#28] Design: transition 시간 값 변경
    
    * [#28] Feat: 이메일 기억하기 로직 수정
    
    * [#28] Feat: 로그인 폼 비어있을 때 로그인 버튼 disabled 처리
    
    * [#28] Design: 로그인 버튼 disabled 스타일 구조 수정
    
    * [#28] Fix: 병합 도중 중복 코드 삭제
    
    * [#28] Feat: 불필요한 주석 삭제
    
    * [#28] Feat: 로그인 정보 없이 페이지 접근되는 오류 수정
    
    * [#28] Feat: user 정보에 따라 특정 페이지로 이동하는 로직 수정
    
    * [#28] Fix: 병합 도중 코드 중복 제거

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SigninPage from '@pages/signin/signin-page'
 import TaskManagementPage from '@pages/task-management/taskManagement-page'
 import TestPage from '@pages/test/test-page'
 import useAuthState from '@hooks/useAuthState'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Loading from '@components/shared/Loading'
 
 function App() {
@@ -16,17 +16,18 @@ function App() {
   if (loading) {
     return <Loading />
   }
+
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
+        <Route element={user ? <Layout /> : <Navigate to="/signin" />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/salary" element={<SalaryManagementPage />} />
           <Route path="/task" element={<TaskManagementPage />} />
           <Route path="/test" element={<TestPage />} />
           <Route path="/admin" element={<AdminPage />} />
         </Route>
-        {!user && <Route path="/signin" element={<SigninPage />} />}
+        <Route path="/signin" element={user ? <Navigate to="/" /> : <SigninPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
